22f6f77d5f12e98c3e2b5b08316ce528f2e9779d
erikn <erik92nordell@gmail.com>
2017-11-23 13:09:32 +0100

    Adds classes to model public transport .

--- SubwayIoT/app/src/main/java/com/example/erikn/subwayiot/Station.java
+++ SubwayIoT/app/src/main/java/com/example/erikn/subwayiot/Station.java
@@ -1,9 +1,24 @@
 package com.example.erikn.subwayiot;
 
+import java.util.LinkedList;
+
 /**
  * Created by erikn on 2017-11-23.
  */
 
 public class Station {
 
+    private String name;
+    private LinkedList<Connection> connections = new LinkedList<>();
+
+    public Station(String name){
+
+    }
+
+    public void addConnection(Connection connection){
+        if(!connections.contains(connection)){
+            connections.add(connection);
+        }
+
+    }
 }
