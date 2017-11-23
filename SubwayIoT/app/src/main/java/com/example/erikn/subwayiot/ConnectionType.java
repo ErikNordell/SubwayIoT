287a9e5f3f664c3224b3816de17001e35c080a6f
erikn <erik92nordell@gmail.com>
2017-11-23 11:49:57 +0100

    Adds classes to model public transport .

---
+++ SubwayIoT/app/src/main/java/com/example/erikn/subwayiot/ConnectionType.java
@@ -0,0 +1,8 @@
+package com.example.erikn.subwayiot;
+
+/**
+ * Created by erikn on 2017-11-23.
+ */
+
+public abstract class ConnectionType {
+}
---
+++ SubwayIoT/app/src/main/java/com/example/erikn/subwayiot/Station.java
@@ -0,0 +1,9 @@
+package com.example.erikn.subwayiot;
+
+/**
+ * Created by erikn on 2017-11-23.
+ */
+
+public class Station {
+
+}
